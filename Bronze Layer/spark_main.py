5e0710a896f7dbfa5429c5e189864a7e8e1c7eda
Bharatp6 <bharatp@gwmail.gwu.edu>
2025-01-11 21:57:18 -0600

    Adding transformation scripts

---
+++ Bronze Layer/spark_main.py
@@ -0,0 +1,142 @@
+from datetime import datetime
+from pyspark.sql import SparkSession
+from delta.tables import DeltaTable
+import pyspark.sql.functions as f
+import great_expectations as gx
+import pandas as pd
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, TimestampType
+from spark_utility import *
+
+
+# Define schema as StructType
+schema_struct = StructType([
+    StructField("transit_timestamp", TimestampType(), True),
+    StructField("transit_mode", StringType(), True),
+    StructField("station_complex_id", StringType(), True),
+    StructField("station_complex", StringType(), True),
+    StructField("borough", StringType(), True),
+    StructField("payment_method", StringType(), True),
+    StructField("fare_class_category", StringType(), True),
+    StructField("ridership", IntegerType(), True),
+    StructField("transfers", IntegerType(), True),
+    StructField("latitude", FloatType(), True),
+    StructField("longitude", FloatType(), True),
+    StructField("georeference", StringType(), True),
+])
+
+##
+expected_columns = ["transit_timestamp", 
+ "transit_mode", 
+ "station_complex_id",
+ "station_complex", 
+ "borough", 
+ "payment_method", 
+ "fare_class_category", 
+ "ridership", 
+ "transfers", 
+ "latitude", 
+ "longitude", 
+ "georeference"]
+
+
+
+spark = SparkSession.builder \
+    .appName("ResourceIssueFix") \
+    .config("spark.executor.instances", "3") \
+    .config("spark.executor.memory", "3g") \
+    .config("spark.executor.cores", "3") \
+    .config("spark.driver.memory", "3g") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .getOrCreate()
+    
+bucket_name = 'mi-buket'
+folder_name = 'nycdata'
+path = f'gs://{bucket_name}/{folder_name}/*.parquet'
+
+
+delta_table_path = f'gs://delta-table-nyc/'
+
+
+df = spark.read.format('parquet').load(path)
+
+delta_presence = check_delta_existance(spark,delta_table_path)
+
+if delta_presence==False:
+    
+    # Perform data validation
+    validation_summary = get_validations('nyc_data','ridership_data','basic_validation','full_data','full_batch',df)
+    schema_check_results = validation_summary.loc[validation_summary['Expectation Type']=='expect_column_to_exist']
+    
+    if all(schema_check_results['Success']):
+        if all(validation_summary['Success'])!=True:
+            good_data,bad_data = data_isolation(df)
+            for field in schema_struct.fields:
+                col_name = field.name
+                col_type = field.dataType
+                good_data = good_data.withColumn(col_name, good_data[col_name].cast(col_type))
+        else:
+            for field in schema_struct.fields:
+                col_name = field.name
+                col_type = field.dataType
+                df = df.withColumn(col_name, df[col_name].cast(col_type))
+                
+            df = df.withColumn('year',f.year(df['transit_timestamp']))
+            
+            
+            # Write the DataFrame to a partitioned Delta table
+            df.write.format("delta") \
+                .mode("overwrite") \
+                .partitionBy("year") \
+                .save(delta_table_path)
+            
+            
+            print('Success')
+    else:
+         raise 'Schema changed'
+            
+else:
+    # Read the Delta table
+    delta_df = spark.read.format("delta").load(delta_table_path)
+    
+    latest_date_delta = delta_df.selectExpr("MAX(transit_timestamp)").collect()[0][0]
+    
+    latest_date_delta = latest_date_delta.strftime("%Y-%m-%dT%H:%M:%S")+ f".{latest_date_delta.microsecond // 1000:03d}"
+    
+    latest_date_source = df.selectExpr("MAX(transit_timestamp)").collect()[0][0]
+    
+    if latest_date_source!=latest_date_delta:
+        print(latest_date_source,latest_date_delta)
+        
+        df_new = df.filter(f.col('transit_timestamp')>latest_date_delta)
+        
+        validation_summary = get_validations('nyc_data','ridership_data','basic_validation','incremental_data','full_batch',df_new)
+        schema_check_results = validation_summary.loc[validation_summary['Expectation Type']=='expect_column_to_exist']
+        
+        # Perform data validation
+        if all(schema_check_results['Success']):
+            if all(validation_summary['Success'])!=True:
+                good_data,bad_data = data_isolation(df_new)
+                for field in schema_struct.fields:
+                    col_name = field.name
+                    col_type = field.dataType
+                    good_data = good_data.withColumn(col_name, good_data[col_name].cast(col_type))
+            else:
+                for field in schema_struct.fields:
+                    col_name = field.name
+                    col_type = field.dataType
+                    df_new = df_new.withColumn(col_name, df_new[col_name].cast(col_type))
+
+                df_new = df_new.withColumn('year',f.year(df_new['transit_timestamp']))
+                
+                # Load the Delta table
+                delta_table = DeltaTable.forPath(spark, delta_table_path)
+                
+                # Perform merge to append only new records
+                delta_table.alias("target").merge(
+                    df_new.alias("source"),
+                    "target.transit_timestamp = source.transit_timestamp AND target.year = source.year"  # Ensure uniqueness with partition column
+                ).whenNotMatchedInsertAll() \
+                 .execute()
+    else:
+        print('Data is upto date')
---
+++ Bronze Layer/spark_utility.py
@@ -0,0 +1,205 @@
+# Perform data validation using Great Expectations
+import great_expectations as gx
+import pandas as pd
+
+def get_validations(data_source_name,data_asset_name,suite_name,batch_definition_name,definition_name,df):
+    
+    global df_
+    global fare_class_cat
+    
+    fare_class_cat = ['Metrocard - Fair Fare',
+       'OMNY - Seniors & Disability',
+       'Metrocard - Seniors & Disability',
+       'Metrocard - Full Fare',
+       'OMNY - Other',
+       'OMNY - Full Fare',
+       'Metrocard - Unlimited 7-Day',
+       'Metrocard - Unlimited 30-Day',
+       'Metrocard - Students',
+       'Metrocard - Other',
+       'OMNY - Students',
+       'OMNY - Fair Fare']
+    
+    df_ = pd.read_csv('https://raw.githubusercontent.com/bharatp666/NYCSubwayAnalytics/updated2/station_data.csv')
+    
+    
+    
+    context = gx.get_context()
+
+    # Define the Data Source name
+
+    # Add the Data Source to the Data Context
+    data_source = context.data_sources.add_spark(name=data_source_name)
+
+    # Define the name of your data asset
+
+    data_asset = data_source.add_dataframe_asset(name=data_asset_name)
+
+    batch_parameters = {"dataframe": df}
+    batch_definition = data_asset.add_batch_definition_whole_dataframe(
+    batch_definition_name
+    )
+
+    batch = batch_definition.get_batch(batch_parameters=batch_parameters)
+
+    # Create an Expectation Suite
+
+    suite = gx.ExpectationSuite(name=suite_name)
+
+    # Add the Expectation Suite to the Data Context
+    suite = context.suites.add(suite)
+    
+    for i in expected_columns:
+        expectation = gx.expectations.ExpectColumnToExist ( 
+            column = i
+        )
+        # Add the previously created Expectation to the Expectation Suite
+        suite.add_expectation(expectation)
+    
+
+    # Create an Expectation to test
+    suite.add_expectation (gx.expectations.ExpectColumnValuesToMatchRegex(
+    column="transit_timestamp",
+    regex=r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}$"
+    ))
+
+
+    suite.add_expectation ( gx.expectations.ExpectColumnValuesToBeInSet(
+    column='transit_mode',
+    value_set=['subway','tram','staten_island_railway']
+    )
+
+                      )
+
+    suite.add_expectation ( gx.expectations.ExpectColumnValuesToBeInSet( 
+    column='station_complex_id',
+    value_set = list(df_['station_complex_id'])
+
+    )
+                      )
+
+    suite.add_expectation ( gx.expectations.ExpectColumnValuesToBeInSet (
+    column='station_complex',
+    value_set = list(df_['station_complex'])
+    )
+                      )
+
+    suite.add_expectation ( gx.expectations.ExpectColumnValuesToBeInSet ( 
+    column = 'borough',
+    value_set = ['Brooklyn','Manhattan','Bronx','Queens','Staten Island']
+    )
+                      )
+
+    suite.add_expectation ( gx.expectations.ExpectColumnValuesToBeInSet (
+    column = 'payment_method',
+    value_set = ['metrocard','omny']
+    )
+                      )
+
+    suite.add_expectation ( gx.expectations.ExpectColumnValuesToBeInSet (
+    column = 'fare_class_category',
+    value_set = fare_class_cat
+    )
+                      )
+
+    suite.add_expectation ( gx.expectations.ExpectColumnValuesToBeBetween(
+    column = 'ridership',
+    min_value=1,
+    max_value=16217
+    )
+                      )
+
+    suite.add_expectation ( gx.expectations.ExpectColumnValuesToBeBetween(
+    column = 'transfers',
+    min_value=0,
+    max_value=1450
+    )
+                      )
+
+    suite.add_expectation ( gx.expectations.ExpectColumnValuesToBeBetween(
+    column = 'latitude',
+    min_value=40.576126,
+    max_value=40.903126
+    )
+                      )
+
+    suite.add_expectation ( gx.expectations.ExpectColumnValuesToBeBetween(
+    column = 'longitude',
+    min_value=-74.07484,
+    max_value=-73.7554
+    )
+                      )
+
+
+    validation_definition = gx.ValidationDefinition(
+    data=batch_definition, suite=suite, name=definition_name
+    )
+
+
+    # Add the Validation Definition to the Data Context
+    validation_definition = context.validation_definitions.add(validation_definition)
+
+    validation_results = batch.validate(suite)
+    
+    results = [
+    {
+        "Expectation Type": result["expectation_config"]["type"],
+        "Column": result["expectation_config"]["kwargs"].get("column", None),
+        "Success": result["success"],
+        "Unexpected Count": result["result"].get("unexpected_count", 0),
+        "Unexpected Percent": result["result"].get("unexpected_percent", 0.0),
+        "Partial Unexpected List": result["result"].get("partial_unexpected_list", []),
+    }
+    for result in validation_results["results"]
+    ]
+
+    # Convert to Pandas DataFrame
+    res_df = pd.DataFrame(results)
+    
+    return res_df
+
+
+# Allowed values and ranges
+
+def data_isolation(df):
+
+    regex_pattern_timestamp = r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}$"
+    allowed_transit_modes = ['subway', 'tram', 'staten_island_railway']
+    allowed_station_complex_ids = list(df_['station_complex_id'])  # Replace with actual values
+    allowed_station_complexes = list(df_['station_complex'])
+    allowed_boroughs = ['Brooklyn', 'Manhattan', 'Bronx', 'Queens', 'Staten Island']
+    allowed_payment_methods = ['metrocard', 'omny']
+    fare_class_cat = fare_class_cat  # Replace with actual values
+    ridership_min, ridership_max = 1, 16217
+    transfers_min, transfers_max = 0, 1450
+    latitude_min, latitude_max = 40.576126, 40.903126
+    longitude_min, longitude_max = -74.07484, -73.7554
+
+    # Combine all conditions
+    bad_records_condition = (
+        ~df["transit_timestamp"].rlike(regex_pattern_timestamp) |
+        ~df["transit_mode"].isin(allowed_transit_modes) |
+        ~df["station_complex_id"].isin(allowed_station_complex_ids) |
+        ~df["station_complex"].isin(allowed_station_complexes) |
+        ~df["borough"].isin(allowed_boroughs) |
+        ~df["payment_method"].isin(allowed_payment_methods) |
+        ~df["fare_class_category"].isin(fare_class_cat) |
+        (df["ridership"] < ridership_min) | (df["ridership"] > ridership_max) |
+        (df["transfers"] < transfers_min) | (df["transfers"] > transfers_max) |
+        (df["latitude"] < latitude_min) | (df["latitude"] > latitude_max) |
+        (df["longitude"] < longitude_min) | (df["longitude"] > longitude_max)
+    )
+
+    # Filter bad records
+    bad_records_df = df.filter(bad_records_condition)
+
+    # Filter good records (optional, if needed)
+    good_records_df = df.filter(~bad_records_condition)
+
+    # Show bad records
+    print(f"Bad Records:{bad_records_df.count()}")
+
+    # Show good records (optional)
+    print(f"Good Records:{good_records_df.count()}")
+    
+    return good_records_df, bad_records_df
