e0b3545572d682a04cfde1acafc31c763b9a8e51
Sowmya Maddali <maddalisowmya82@gmail.com>
2025-07-21 22:45:28 -0400

    Updating bronze_layer code

--- bronze_layer/main.py
+++ bronze_layer/main.py
@@ -1,61 +1,34 @@
-from functions import *
+import requests
 import polars as pl
-import os
-import time
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import polars as pl
 from joblib import Parallel, delayed
-from tqdm import tqdm
-from logger_init import *  # your initialized logger
+import re
 from google.cloud import storage
-
-
+from functions import *
 
 
 if __name__ == "__main__":
     # Configuration
     BUCKET_NAME = os.getenv("BUCKET_NAME")
     FOLDER_NAME = os.getenv("FOLDER_NAME")
+    N_JOBS = os.getenv("N_JOBS")
 
     if not BUCKET_NAME or not FOLDER_NAME:
         raise ValueError("Environment variables BUCKET_NAME and FOLDER_NAME are required.")
-
-
-    last_date = check_gcs_parquet_files(BUCKET_NAME,FOLDER_NAME)
-    min_date, max_date = fetch_min_max_dates()
-
-    if last_date is None:
-        date_ranges = split_date_range(min_date, max_date)
-        labelled_dates = label_chunks_by_time(date_ranges)
-        str_labelled_dates = convert_timechunks_to_strings(labelled_dates)  
-
-        
-        start_time = time.time()
-
-        results = Parallel(n_jobs=3)(
-            delayed(fetch_and_write_bulk)(start, end, label,BUCKET_NAME,FOLDER_NAME)
-            for start, end, label in tqdm(str_labelled_dates, desc="Processing")
-        )
-
-        for res in results:
-            print(res)
-
-        total_time = time.time() - start_time
-        print(f"\n Total time taken: {total_time:.2f} seconds")
     
-    else:
-        date_ranges = split_date_range(last_date,max_date)
-        labelled_dates = label_chunks_by_time(date_ranges)
-        str_labelled_dates = convert_timechunks_to_strings(labelled_dates)  
+    source_timestamps = get_source_dates()
+    destination_timestamps = get_existing_datetimes_from_gcs(BUCKET_NAME,FOLDER_NAME)
+    delta_timestamps = list(source_timestamps - destination_timestamps)
+    if delta_timestamps:
+        fetch_timestamps =[]
+        for i in delta_timestamps:
+            fetch_timestamps.append((i,increment_month(i)))
 
-        start_time = time.time()
-
-        results = Parallel(n_jobs=3)(
-            delayed(fetch_and_write_incremental)(start, end, label, BUCKET_NAME, FOLDER_NAME)
-            for start, end, label in tqdm(str_labelled_dates, desc="Processing")
+        results = Parallel(N_JOBS)(
+            delayed(fetch_and_write)(start, end, BUCKET_NAME, FOLDER_NAME)
+            for start, end in fetch_timestamps
         )
-
-        for res in results:
-            print(res)
-
-        total_time = time.time() - start_time
-        print(f"\n Total time taken: {total_time:.2f} seconds")
-
+    else:
+        gcp_logger.log_text('Data is upto date', severity="INFO")
