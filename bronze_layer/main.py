4033fb180e506464d7bbdfc1dcd7cd25a6f1fd1f
bharatp666 <p3311598@gmail.com>
2025-07-27 16:32:23 -0400

    Update main.py

--- bronze_layer/main.py
+++ bronze_layer/main.py
@@ -34,6 +34,7 @@ from functions import *
 if __name__ == "__main__":
     BUCKET_NAME = os.getenv("BUCKET_NAME")
     FOLDER_NAME = os.getenv("FOLDER_NAME")
+    N_JOBS = os.getenv("N_JOBS")
     
     # From Cloud Run injected env vars
     TASK_INDEX = int(os.getenv("CLOUD_RUN_TASK_INDEX", "0"))
@@ -42,17 +43,12 @@ if __name__ == "__main__":
     source_timestamps = get_source_dates()
     destination_timestamps = get_existing_datetimes_from_gcs(BUCKET_NAME, FOLDER_NAME)
     delta_timestamps = sorted(list(source_timestamps - destination_timestamps))
+    
 
     if not delta_timestamps:
         gcp_logger.log_text('Data is up to date', severity="INFO")
         exit(0)
 
-    # Partition delta_timestamps across tasks
-    # total = len(delta_timestamps)
-    # chunk_size = (total + TASK_COUNT - 1) // TASK_COUNT  # ceiling division
-    # start_idx = TASK_INDEX * chunk_size
-    # end_idx = min(start_idx + chunk_size, total)
-    # my_chunk = delta_timestamps[start_idx:end_idx]
     # Task-aware chunk distribution (round-robin)
     my_chunk = [ts for i, ts in enumerate(delta_timestamps) if i % TASK_COUNT == TASK_INDEX]
 
@@ -64,6 +60,7 @@ if __name__ == "__main__":
     fetch_timestamps = [(i, increment_month(i)) for i in my_chunk]
 
 
-    for start, end in fetch_timestamps:
-        fetch_and_write(start, end, BUCKET_NAME, FOLDER_NAME)
+    Parallel(N_JOBS)(
+             delayed(fetch_and_write)(start, end, BUCKET_NAME, FOLDER_NAME)
+             for start, end in fetch_timestamps
 
