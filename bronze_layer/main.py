2933b153f299f039a6829166d0206cb2db59fe9e
bharatp666 <p3311598@gmail.com>
2025-07-27 13:21:18 -0400

    Update main.py

--- bronze_layer/main.py
+++ bronze_layer/main.py
@@ -34,7 +34,6 @@ from functions import *
 if __name__ == "__main__":
     BUCKET_NAME = os.getenv("BUCKET_NAME")
     FOLDER_NAME = os.getenv("FOLDER_NAME")
-    N_JOBS = os.getenv("N_JOBS")
     
     # From Cloud Run injected env vars
     TASK_INDEX = int(os.getenv("CLOUD_RUN_TASK_INDEX", "0"))
@@ -64,8 +63,7 @@ if __name__ == "__main__":
 
     fetch_timestamps = [(i, increment_month(i)) for i in my_chunk]
 
-    Parallel(n_jobs=N_JOBS)(
-        delayed(fetch_and_write)(start, end, BUCKET_NAME, FOLDER_NAME)
-        for start, end in fetch_timestamps
-    )
+
+    for start, end in fetch_timestamps:
+        fetch_and_write(start, end, BUCKET_NAME, FOLDER_NAME)
 
